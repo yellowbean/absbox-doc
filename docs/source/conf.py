002614b109d9cc6c6f4264eaf454dbe909b47b79
xiaoyu <always.zhang@gmail.com>
2023-05-06 23:07:02 +0800

    using sample config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,16 @@ intersphinx_mapping = {
 }
 
 html_theme_options = {
-    'navigation_depth':-1,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
     'collapse_navigation': True,
-    'titles_only':False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False    
 }
 
 
